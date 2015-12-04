e21d1ea09c3ed531208883beea9d6c31a7e37dea
GavinChangCN <gavinchangcn@163.com>
2015-12-04 11:37:18 +0800

    这个完了添加了 - -!

--- app/src/main/java/com/gavin/amazingcamera/widget/MySurfaceView.java
+++ app/src/main/java/com/gavin/amazingcamera/widget/MySurfaceView.java
@@ -1,9 +1,14 @@
 package com.gavin.amazingcamera.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.util.TypedValue;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.List;
 
 /**
  * Author: Gavin
@@ -12,16 +17,22 @@ import android.view.SurfaceView;
  */
 public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
-
-    private Camera camera = null;
+    private Context mContext;
+    private Resources mResources;
+    private Camera mCamera = null;
     private SurfaceHolder surfaceHolder = null;
+    private List<Size> mSupportedPreviewSizes;
+    private Size mPreviewSize;
 
     public MySurfaceView(Context context, Camera camera) {
         super(context);
-        this.camera = camera;
+        this.mContext = context;
+        this.mCamera = camera;
+        this.mResources = mContext.getResources();
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
     }
     public MySurfaceView(Context context) {
         super(context);
@@ -31,8 +42,8 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         try{
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -48,20 +59,27 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
         //先停止Camera的预览
         try{
-            camera.stopPreview();
+            mCamera.stopPreview();
         }catch(Exception e){
             e.printStackTrace();
         }
 
-        //这里可以做一些我们要做的变换。
+        // 这里可以做变换。
+        if(mCamera != null){
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+            mCamera.setParameters(parameters);
+        }
 
         //重新开启Camera的预览功能
         try{
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
         }catch(Exception e){
             e.printStackTrace();
         }
+
+
     }
 
     @Override
@@ -69,4 +87,52 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     }
 
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+//        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+//        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        Integer widthdp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1024, mResources.getDisplayMetrics());
+        Integer heightdp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 768, mResources.getDisplayMetrics());
+        final int width = widthdp;
+        final int height = heightdp;
+        setMeasuredDimension(width, height);
+
+        if (mSupportedPreviewSizes != null) {
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        }
+    }
 }
